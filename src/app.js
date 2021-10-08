d8f6c1f22b2a672a363f1b3d12a4c71a50cd9c42
DarioDemix <dario.demarco1208@gmail.com>
2021-10-08 23:14:16 +0200

    Removed play function

--- src/app.js
+++ src/app.js
@@ -4,15 +4,12 @@ const { rl } = require('./readLine');
 async function start() {
     let wantToContinue = 'y';
     while (wantToContinue !== 'n') {
-        await play();
+        const numberOfDisks = await rl.ask("Insert the number of disks: ");
+        towerOfHanoi(numberOfDisks);
         wantToContinue = await rl.ask("Continue? (y/n): ")
     }
     rl.close();
 
-    async function play() {
-        const numberOfDisks = await rl.ask("Insert the number of disks: ");
-        towerOfHanoi(numberOfDisks);
-    }
 }
 
 start();
